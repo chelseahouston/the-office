073d6cc92c0240ea8638f3e7f2244f53219f307b
Chelsea Houston <chelseahouston29@gmail.com>
2024-05-14 09:04:37 +0100

    dialogue sections

--- Assets/Scripts/Dialogue/DialogueDatabase.cs
+++ Assets/Scripts/Dialogue/DialogueDatabase.cs
@@ -18,14 +18,43 @@ public class DialogueDatabase : MonoBehaviour
     public void ConfigureDialogue()
     {
         dialogueDB.Clear();
+        // looking at desks
         dialogueDB.Add(new Dialogue(1, "Player", "This is John's desk.", "Search desk", "Do nothing"));
         dialogueDB.Add(new Dialogue(2, "Player", "This is Jane's desk.", "Search desk", "Do nothing"));
         dialogueDB.Add(new Dialogue(3, "Player", "This is Sam's desk.", "Search desk", "Do nothing"));
         dialogueDB.Add(new Dialogue(4, "Player", "This is Emily's desk.", "Search desk", "Do nothing"));
         dialogueDB.Add(new Dialogue(5, "Player", "This is Charlie's desk.", "Search desk", "Do nothing"));
+
+        // coworkers present, dont serach
         dialogueDB.Add(new Dialogue(6, "Player", "It's not a good idea to search people's desks with colleages in the room.", "Good call...", ""));
+
+        // initial dialogues with each NPC
         dialogueDB.Add(new Dialogue(7, "Jane", "Ugh, Monday already huh? What do you want?", "Where is everyone today?", "Say nothing"));
         dialogueDB.Add(new Dialogue(8, "John", "Ready for the big presentation today, mate?", "I was born ready!", "Not at all..."));
+        dialogueDB.Add(new Dialogue(9, "Emily", "", "Option1", "Option2"));
+        dialogueDB.Add(new Dialogue(10, "Sam", "", "Option1", "Option2"));
+        dialogueDB.Add(new Dialogue(11, "Charlie", "", "Option1", "Option2"));
+        dialogueDB.Add(new Dialogue(10, "Boss", "", "Option1", "Option2"));
+
+        // can search desks
+        dialogueDB.Add(new Dialogue(11, "Player", "There's a ITEM in here", "Take", "Leave"));
+        dialogueDB.Add(new Dialogue(12, "Player", "There's a ITEM in here", "Take", "Leave"));
+        dialogueDB.Add(new Dialogue(13, "Player", "There's a ITEM in here", "Take", "Leave"));
+        dialogueDB.Add(new Dialogue(14, "Player", "There's a ITEM in here", "Take", "Leave"));
+        dialogueDB.Add(new Dialogue(15, "Player", "There's a ITEM in here", "Take", "Leave"));
+
+        // scene 1 dialogue - main office / storage closet
+
+        // scene 2 dialogue - presentation / conference room
+
+        // scene 3 dialogue - lunch cafeteria
+
+        // scene 4 dialogue - bathroom (lunch break)
+
+        // scene 5 dialogue - main office / boss office (lunch break)
+
+        // scene 6 dialogue - main office final scene
+
     }
 
     public string GetNameByID(int id)
