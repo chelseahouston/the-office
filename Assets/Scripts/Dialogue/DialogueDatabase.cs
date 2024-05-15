85707dbcb335f3d3d47488a7a613008e0a5436dc
Chelsea Houston <chelseahouston29@gmail.com>
2024-05-15 09:07:21 +0100

    scene1 dialogue added

--- Assets/Scripts/Dialogue/DialogueDatabase.cs
+++ Assets/Scripts/Dialogue/DialogueDatabase.cs
@@ -24,8 +24,7 @@ public class DialogueDatabase : MonoBehaviour
         dialogueDB.Add(new Dialogue(3, "Player", "This is Sam's desk.", "Search desk", "Do nothing"));
         dialogueDB.Add(new Dialogue(4, "Player", "This is Emily's desk.", "Search desk", "Do nothing"));
         dialogueDB.Add(new Dialogue(5, "Player", "This is Charlie's desk.", "Search desk", "Do nothing"));
-
-        // coworkers present, dont serach
+        // coworkers present, don't search
         dialogueDB.Add(new Dialogue(6, "Player", "It's not a good idea to search people's desks with colleages in the room.", "Good call...", ""));
 
         // initial dialogues with each NPC
@@ -37,13 +36,32 @@ public class DialogueDatabase : MonoBehaviour
         dialogueDB.Add(new Dialogue(10, "Boss", "", "Option1", "Option2"));
 
         // can search desks
-        dialogueDB.Add(new Dialogue(11, "Player", "There's a ITEM in here", "Take", "Leave"));
-        dialogueDB.Add(new Dialogue(12, "Player", "There's a ITEM in here", "Take", "Leave"));
-        dialogueDB.Add(new Dialogue(13, "Player", "There's a ITEM in here", "Take", "Leave"));
-        dialogueDB.Add(new Dialogue(14, "Player", "There's a ITEM in here", "Take", "Leave"));
-        dialogueDB.Add(new Dialogue(15, "Player", "There's a ITEM in here", "Take", "Leave"));
+        dialogueDB.Add(new Dialogue(11, "Player", "There's an ERASER in here", "Take it", "Leave it")); // John's Desk
+        dialogueDB.Add(new Dialogue(12, "Player", "There's a PAPERCLIP in here", "Take it", "Leave it")); // Jane's Desk
+        dialogueDB.Add(new Dialogue(13, "Player", "There's a USB in here", "Take it", "Leave it")); // Sam's Desk
+        dialogueDB.Add(new Dialogue(14, "Player", "There's DUCT TAPE in here", "Take it", "Leave it")); // Charlie's Desk
+        dialogueDB.Add(new Dialogue(15, "Player", "There's $50 in here", "Take it", "Leave it")); // Emily's Desk
 
         // scene 1 dialogue - main office / storage closet
+        // Jane
+        dialogueDB.Add(new Dialogue(16, "Jane", "John is over there. He knows more than me.", "...Oh ok", ""));
+        dialogueDB.Add(new Dialogue(17, "Jane", "Oh.. maybe the conference room. Probably time I head in there too.", "Cool, see you in there!", ""));
+        // John
+        dialogueDB.Add(new Dialogue(18, "John", "I have some special candy in my car that might help, be right back!", "...uhh", ""));
+        dialogueDB.Add(new Dialogue(19, "John", "Awesome. Good luck!", "Thanks!", ""));
+        // Emily
+        dialogueDB.Add(new Dialogue(20, "Emily", "I'm so glad you're here! I can't reach the printer paper in the storage room. Can you help me, please?", "Sure, I'll come and help.", "Sorry, I can't right now."));
+        dialogueDB.Add(new Dialogue(21, "Emily", "Time for the REAL assistant manager to give your... I mean MY presentation.", "...!?!", ""));
+        // Player (storage closet)
+        dialogueDB.Add(new Dialogue(22, "Player", "She's actually locked me in here...", "I need to get out!", ""));
+        dialogueDB.Add(new Dialogue(23, "Player", "Oh this looks like the keycard to the Boss' office", "Take it", "Leave it"));
+        dialogueDB.Add(new Dialogue(24, "Player", "I could probably pick this lock if I had the right tools", "I believe I have the tools", "I don't have the tools, I'll have to wait until I'm let out"));
+        dialogueDB.Add(new Dialogue(25, "Player", "Ah the lockpick broke! Perhaps I could have made it sturdier.", "Time to get outa here!", ""));
+        // didn't go into storage closet
+        dialogueDB.Add(new Dialogue(26, "Emily", "Ugh.. Rude.", "...", ""));
+        dialogueDB.Add(new Dialogue(27, "Sam", "Hey! Did you hear the rumour?", "What rumour?", ""));
+        dialogueDB.Add(new Dialogue(28, "Sam", "Apparently the Boss is hooking up with Emily! Not that there's anything wrong with office relationships in my opinion. What's your take on them?", "I have no problem with them.", "It's not something I agree with."));
+        dialogueDB.Add(new Dialogue(29, "Sam", "So in that case, would you like to have lunch together today?", "It's a date", "I'm flattered but no, thank you."));
 
         // scene 2 dialogue - presentation / conference room
 
